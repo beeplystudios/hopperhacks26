c64880fb69cdbc73939dbc8f50981d373ecb2803
Gilbert Zhang <gilbertzhang1208@gmail.com>
2026-02-21 19:40:57 -0500

    create routers that should exist

--- src/server/trpc/routes/index.ts
+++ src/server/trpc/routes/index.ts
@@ -1,6 +1,8 @@
 import { extractAuth } from "../middleware/auth-middleware";
 import { publicProcedure, router } from "../trpc-config";
+import { menuRouter } from "./menu";
 import { restaurantRouter } from "./restaurant";
+import { tableRouter } from "./table";
 
 export const appRouter = router({
   test: publicProcedure.query(async () => "Hi from the server!"),
@@ -9,7 +11,9 @@ export const appRouter = router({
     .use(extractAuth)
     .query(async ({ ctx }) => ctx.user ?? null),
 
+  table: tableRouter,
   restaurant: restaurantRouter,
+  menu: menuRouter,
 });
 
 // Export type router type signature,
