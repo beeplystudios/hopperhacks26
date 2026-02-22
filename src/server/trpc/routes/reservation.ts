a0a788c231969762645ab0c21f1954dadd9cb0ea
SantiagoVira <santiagovira06@gmail.com>
2026-02-21 21:09:10 -0500

    createPending reservation

--- src/server/trpc/routes/reservation.ts
+++ src/server/trpc/routes/reservation.ts
@@ -3,6 +3,7 @@ import { db } from "@/server/db";
 import { reservation, restaurant, table } from "@/server/db/schema";
 import { z } from "zod";
 import { and, asc, eq, gt, gte, lt } from "drizzle-orm";
+import { authedProcedure } from "../middleware/auth-middleware";
 
 const timeStringToMinutes = (time: string) => {
   const [hours, minutes] = time.split(":").map(Number);
@@ -17,6 +18,28 @@ const minutesToTimeString = (minutes: number) => {
 };
 
 export const reservationRouter = router({
+  createPending: authedProcedure
+    .input(
+      z.object({
+        restaurantId: z.string(),
+        tableId: z.string(),
+        startTime: z.date(),
+        endTime: z.date(),
+        numberOfSeats: z.number(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const newReservation = await db.insert(reservation).values({
+        restaurantId: input.restaurantId,
+        tableId: input.tableId,
+        startTime: input.startTime,
+        endTime: input.endTime,
+        numberOfSeats: input.numberOfSeats,
+        status: "PENDING",
+        userId: ctx.user.id,
+      });
+      return newReservation;
+    }),
   getAvailableTimes: publicProcedure
     .input(
       z.object({
