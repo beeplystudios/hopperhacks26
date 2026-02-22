8f717a471272423e75afd05717c00838c2c05415
SantiagoVira <santiagovira06@gmail.com>
2026-02-21 22:19:44 -0500

    temp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import RestaurantCard from "@/components/pages/Restaurants/RestaurantCard";
 import { signInOptions, signOutOptions } from "@/lib/auth-client";
 import { useTRPC } from "@/lib/trpc-client";
+import { TRPCRouter } from "@/server/trpc/routes";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { inferRouterOutputs } from "@trpc/server";
 import { signInSocial, signOut } from "better-auth/api";
 
 export const Route = createFileRoute("/")({
@@ -20,17 +22,15 @@ function App() {
   return (
     <div className="text-center flex flex-col items-center w-full py-8">
       <h2 className="text-6xl font-bold text-left w-full px-8">Welcome!</h2>
-      <div className="flex items-center w-full px-6 overflow-scroll gap-4 py-6">
-        {restrauntQuery.data?.map((r) => (
-          <RestaurantCard
-            key={r.id}
-            title={r.name}
-            description={r.description ?? ""}
-            logo={r.logoImage}
-            id={r.id}
-          />
-        ))}
-      </div>
+
+      <p className="text-left">Order Again</p>
+      <RestaurantRow restaurants={restrauntQuery.data ?? []} />
+
+      <p className="text-left"></p>
+      <RestaurantRow restaurants={restrauntQuery.data ?? []} />
+
+      <p className="text-left">Explore All</p>
+      <RestaurantRow restaurants={restrauntQuery.data ?? []} />
 
       <button onClick={() => signIn.mutate()}>Sign In</button>
       <p>{JSON.stringify(user.data)}</p>
@@ -39,4 +39,18 @@ function App() {
   );
 }
 
-// const RestaurantRow = (restaurants: )
+const RestaurantRow: React.FC<{
+  restaurants: inferRouterOutputs<TRPCRouter>["restaurant"]["getAll"];
+}> = ({ restaurants }) => (
+  <div className="flex items-center w-full px-6 overflow-scroll gap-4 py-6">
+    {restaurants.map((r) => (
+      <RestaurantCard
+        key={r.id}
+        title={r.name}
+        description={r.description ?? ""}
+        logo={r.logoImage}
+        id={r.id}
+      />
+    ))}
+  </div>
+);
